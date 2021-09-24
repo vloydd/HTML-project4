61986dca3546935d37fc254301a69606b42cde67
vloydd <ma7kitan@gmail.com>
2021-09-24 10:53:37 +0300

    24.09 set up gulp sass compiler

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
+
+gulp.task('compile', function(){
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error' ,sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css/'))
+
+})
+
+gulp.task('watch', function(){
+    gulp.watch('./scss/**/*.scss', ['compile'])
+
+})
